cd1a4ebab7adf547a83e18ebd65f5a403f56c460
samibrahmi <samibrahmi@yahoo.fr>
2018-05-10 01:37:16 +0200

    This is the intitial commit of my first android kotlin program

---
+++ app/src/main/java/com/kotlin/ping/firstkotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kotlin.ping.firstkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txtHello.text="hello sami"
+    }
+}
